e984e31f1e3a3fd3584372e912884fa566477d21
zhuoyangjiang <70708249+zhuoyangjiang@users.noreply.github.com>
2020-09-16 15:27:43 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,49 @@
+// Author: zhuoyangjiang zfj5038@psu.edu
+// Collaborator:
+// Collaborator:
+// Collaborator:
+// Section: 007R
+// Breakout: 
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int sum_n(int n)
+{
+    if(n<=0)
+    {
+        return n;
+    }
+
+    return n + sum_n(n-1);   
+}
+
+void print_n(const char *s, int n)
+{
+    if(n<=0)
+    {
+        return ;
+    }
+    else
+    {
+        print_n(s,n-1);
+        printf("%s\n", s);
+    }
+}
+
+int main()
+{
+    int n=0;
+    char *str;
+    printf("Enter an int:");
+    scanf("%d", &n);
+    printf("sum is %d.\n", sum_n(n));
+
+    str = readline("Enter a string:");
+    print_n(str,n);
+
+    free(str);
+    return 0;
+}
